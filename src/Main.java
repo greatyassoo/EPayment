47acb1a4b1255626311f8da5e51e2a2180995aa8
Fares Karim <faressskarim@gmail.com>
2022-11-28 20:04:26 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        FileManagement.GetInstance();
+        FileManagement f = FileManagement.GetInstance();
         try{
-            //f.SignUp("Faris","i-likem-wet-69","0123456789","Faris-tneen@fci.lol");
+            f.SignUp("Faris","i-likem-wet-69","0123456789","Faris-tneen@fci.lol");
             Account x = FileManagement.GetInstance().LogIn("Faris","123");
             System.out.println(x.email);
         }catch(Exception e){};
