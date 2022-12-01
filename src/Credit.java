a6ef073fc1abed35597faf038da89c69448b43a2
Karim Lialy <96483467+Kemol001@users.noreply.github.com>
2022-12-01 20:30:46 +0200

    Update Credit.java

--- src/Credit.java
+++ src/Credit.java
@@ -2,10 +2,10 @@
 
 public class Credit implements Payment{
 
-	@Override
-	public boolean Pay(int amount) {
-		
-		return false;
-	}
-
+    @Override
+    public boolean Pay(int amount) {
+        System.out.println("Payment Done Using Credit Card");
+        return true;
+    }
+    
 }
