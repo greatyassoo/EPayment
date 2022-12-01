6d30cb767fc2bdc60abbe34222f753e33e20b222
Karim Lialy <96483467+Kemol001@users.noreply.github.com>
2022-12-01 21:57:11 +0200

    Update ServiceProvider.java

--- src/ServiceProvider.java
+++ src/ServiceProvider.java
@@ -1,12 +1,22 @@
+import java.util.LinkedList;
+
 public class ServiceProvider {
-    private String name;
+    private String sname,pname;
+    private LinkedList<String> Data = new LinkedList<String>();
     private Form form;
     private Payment paymMethod;
-    ServiceProvider(String name,Form form){
-        this.name=name;
+    ServiceProvider(String gsname,String gpname,LinkedList<String> gdata,Form form){
+        this.sname=gsname;
+        this.pname=gpname;
+        this.Data=gdata;
         this.form=form;
+    }
+    public boolean handleRequest(){
+        Transaction ntransaction=new Transaction(sname, pname,Data, 0);
+
     }
     public void setPaymentMethod(Payment py){
         this.paymMethod=py;
+        paymMethod.Pay(0);
     }
 }
