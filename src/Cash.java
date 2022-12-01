aa8f59723e11f6195e89ce893a46946c3f69f903
Karim Lialy <96483467+Kemol001@users.noreply.github.com>
2022-12-01 20:31:37 +0200

    Update Cash.java

--- src/Cash.java
+++ src/Cash.java
@@ -2,8 +2,10 @@
 
 public class Cash implements Payment{
 
-	@Override
-	public boolean Pay(int amount) {
-		return false;
-	}
+    @Override
+    public boolean Pay(int amount) {
+        System.out.println("Payment Done Using Cash On Delivery");
+        return true;
+    }
+    
 }
