31cc1687d9acd391ced16ef57d80a50796f36ca8
Karim Lialy <96483467+Kemol001@users.noreply.github.com>
2022-12-01 20:27:18 +0200

    Update Wallet.java

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,9 +1,11 @@
 
 
-public class Wallet implements Payment{
+ublic class Wallet implements Payment{
 
-	@Override
-	public boolean Pay(int amount) {
-		return false;
-	}
+    @Override
+    public boolean Pay(int amount) {
+        System.out.println("Payment Done Using Cash from Wallet");
+        return true;
+    }
+    
 }
